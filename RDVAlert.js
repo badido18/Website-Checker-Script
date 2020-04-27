7b1538c85cea424589ce5b2e1960c343b3144c5e
Mohamed Abdelmadjid Boudis <34186054+badido18@users.noreply.github.com>
2020-04-27 21:49:58 +0100

    Feat : async request

--- RDVAlert.js
+++ RDVAlert.js
@@ -3,21 +3,25 @@ const open = require('open');
 console.log('checking ...  ');
 var html1, html2;
 
-request('https://algeria.blsspainvisa.com/book_appointment.php',(error,response,html)=>{
-    if(!error){
-        html1 = html;
-        setInterval(() => {
-            request('https://algeria.blsspainvisa.com/book_appointment.php',(error, response, body) => {
-            if (!error) {
-                html2 = body;
-                if (html1 != html2) {
-                    console.log("changed");
-                    open('https://algeria.blsspainvisa.com/book_appointment.php');
-		html1=html2;
+const verif = async () =>{
+	await request('https://algeria.blsspainvisa.com/book_appointment.php',(error,response,html)=>{
+	    if(!error){
+		html1 = html;
+		setInterval(() => {
+		    await request('https://algeria.blsspainvisa.com/book_appointment.php',(error, response, body) => {
+		    if (!error) {
+			html2 = body;
+			if (html1 != html2) {
+			    console.log("changed");
+			    open('https://algeria.blsspainvisa.com/book_appointment.php');
+			html1=html2;
 
-                }
-            }
-        });
-    },1000);
-        }   
-}); 
+			}
+		    }
+		});
+	    },1000);
+		}   
+	}); 
+}
+
+verif()
